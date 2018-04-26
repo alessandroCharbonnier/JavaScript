74fec2067e974d8d590b273d508d43ede20400e6
alessandro <haloalessandro230@gmail.com>
2018-04-26 10:08:35 +0200

    better visual

--- rain/drops.js
+++ rain/drops.js
@@ -2,21 +2,26 @@ var drops = []
 
 class Drop {
 	constructor() {
-		this.x = random(width)
-		this.y = random(-height)
-		this.yspeed = random(8, 15)
-		this.len = random(4, 17)
+		this.gravity = createVector(0, 0.12);
+		this.pos = createVector(random(width), random(-2*height, 0))
+		this.speed = createVector(0, random(4, 10));
+		this.acc = createVector(0, 0);
+		this.len = 0;
 	}
 
 	fall() {
-		this.y += this.yspeed
+		this.acc.add(this.gravity)
+		this.speed.add(this.acc);
+		this.pos.add(this.speed);
+		this.acc.mult(0);
+		this.len = map(this.speed.y, 0.12, 15, 4, 17);
+		if (this.pos.y > height) {
+			this.pos.y = -height;
+			this.speed = createVector(0, random(4, 10));;
+		}
 	}
 
 	show() {
-		fill(10, 215, 255, 100)
-		noStroke()
-		rect(this.x, this.y, 1, this.len)
-		if (this.y - this.len > height)
-			this.y = random(-height)
+		rect(this.pos.x, this.pos.y, 1, this.len);
 	}
 }
