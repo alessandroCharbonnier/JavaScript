62a37c878ad440164f1a627bc11155eaff5924cb
alessandro <haloalessandro230@gmail.com>
2018-04-26 10:47:54 +0200

    faster

--- phyllotaxis/sketch.js
+++ phyllotaxis/sketch.js
@@ -5,27 +5,29 @@ let a = 137.5;
 let array = [];
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(0);
-  angleMode(DEGREES);
-  colorMode(HSB);
+	createCanvas(windowWidth, windowHeight);
+	background(0);
+	angleMode(DEGREES);
+	colorMode(HSB);
 }
 
 function draw() {
-  background(0);
-  translate(width / 2, height / 2);
-  let angle = n * a;
-  let r = c * sqrt(angle);
-  let x = r * cos(angle);
-  let y = r * sin(angle);
-  n++;
-  array.push(new Seed(x, y, (angle - n) % 361, 255, 255));
+	background(0);
+	translate(width / 2, height / 2);
+	for (let i = 0; i < 100; i++) {
+		let angle = n * a;
+		let r = c * sqrt(angle);
+		let x = r * cos(angle);
+		let y = r * sin(angle);
+		n++;
+		array.push(new Seed(x, y, (angle - n) % 361, 255, 255));
+	}
 
-  for(let seed of array) {
-    seed.display();
-  }
+	for (let seed of array) {
+		seed.display();
+	}
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+	resizeCanvas(windowWidth, windowHeight);
 }
