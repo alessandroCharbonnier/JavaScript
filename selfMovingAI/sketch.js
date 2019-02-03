5f757bec7bf9edcb8e04c227ea17a2268e1a90df
charbonna <alessandro.charbonnier@etu.u-pec.fr>
2019-02-03 18:51:35 +0100

    first commit

---
+++ selfMovingAI/sketch.js
@@ -0,0 +1,35 @@
+let qtree;
+let vehicules = [];
+const NUMVEHICULES = 60;
+let maxDist;
+
+function setup() {
+	frameRate(60);
+	createCanvas(windowWidth, windowHeight);
+	maxDist = dist(0, 0, width, height);
+	for (let i = 0; i < NUMVEHICULES; i++) {
+		vehicules.push(new Vehicule(random(width), random(height)));
+	}
+}
+
+function draw() {
+	// clear background
+	background(0);
+
+	// draw all vehicules
+	for (let i = vehicules.length - 1; i >= 0; i--) {
+		const vehicule = vehicules[i];
+		vehicule.update();
+		vehicule.draw();
+	}
+
+	// draw informations
+	fill(255);
+	noStroke();
+	text("fps : " + floor(frameRate()), 10, 15);
+}
+
+function windowResized() {
+	resizeCanvas(windowWidth, windowHeight);
+	maxDist = dist(0, 0, width, height);
+}
